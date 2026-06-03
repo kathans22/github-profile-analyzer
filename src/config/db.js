fa6d97104f042638764b407f0ec61d9c9ec44499
kathans22 <kathanshah2215@gmail.com>
2026-06-03 18:24:22 +0530

    console -> db.js

--- src/config/db.js
+++ src/config/db.js
@@ -142,6 +142,11 @@ function getPool() {
       queueLimit: 0,
       timezone: "+00:00",
     });
+
+    console.log("DB_HOST =", process.env.DB_HOST);
+    console.log("DB_PORT =", process.env.DB_PORT);
+    console.log("DB_USER =", process.env.DB_USER);
+    console.log("DB_NAME =", process.env.DB_NAME);
   }
   return pool;
 }
