efe6e80bee685a5ed5119a8da6b289fc4e181777
kathans22 <kathanshah2215@gmail.com>
2026-06-03 18:46:33 +0530

    console -> server.js v1

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const db = require("./src/config/db");
 
 const PORT = process.env.PORT || 3000;
 
+console.log("DB_HOST:", JSON.stringify(process.env.DB_HOST));
+console.log("DB_PORT:", JSON.stringify(process.env.DB_PORT));
+console.log("DB_USER:", JSON.stringify(process.env.DB_USER));
+console.log("DB_NAME:", JSON.stringify(process.env.DB_NAME));
+
+console.log("MYSQLHOST:", JSON.stringify(process.env.MYSQLHOST));
+console.log("MYSQLPORT:", JSON.stringify(process.env.MYSQLPORT));
+console.log("MYSQLUSER:", JSON.stringify(process.env.MYSQLUSER));
+console.log("MYSQLDATABASE:", JSON.stringify(process.env.MYSQLDATABASE));
+
+
 async function bootstrap() {
   try {
     await db.testConnection();
--- src/config/db.js
+++ src/config/db.js
@@ -143,12 +143,14 @@ function getPool() {
       timezone: "+00:00",
     });
 
-    console.log("=== ENV CHECK ===");
-    console.log("DB_HOST:", process.env.DB_HOST);
-    console.log("DB_PORT:", process.env.DB_PORT);
-    console.log("DB_USER:", process.env.DB_USER);
-    console.log("DB_NAME:", process.env.DB_NAME);
-    console.log("=================");
+    // console.log("=== ENV CHECK ===");
+    // console.log("DB_HOST:", process.env.DB_HOST);
+    // console.log("DB_PORT:", process.env.DB_PORT);
+    // console.log("DB_USER:", process.env.DB_USER);
+    // console.log("DB_NAME:", process.env.DB_NAME);
+    // console.log("=================");
+
+    
   }
   return pool;
 }
